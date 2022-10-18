af0c16dd6240503e6ca20620e14179ff55b2e4d1
Yeva22 <y.shimanayev@gmail.com>
2022-10-17 20:32:41 -0400

    New test for user story Ex145, added test for logIn

--- src/Test1.java
+++ src/Test1.java
@@ -1,6 +1,7 @@
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("Test 1");
-
+        System.out.println("Test 2");
+        System.out.println("Test 5");
     }
 }
